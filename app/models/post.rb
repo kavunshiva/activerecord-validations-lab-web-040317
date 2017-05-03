e0ece2b10e1440311e3d9ec5d6cf2a390ea99710
kavunshiva <kavunshiva@gmail.com>
2017-05-03 11:03:01 -0400

    regex trix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :title, format: {with: %r{(Won't Believe|Secret|Top [0-9]|Guess)}}
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
 end
